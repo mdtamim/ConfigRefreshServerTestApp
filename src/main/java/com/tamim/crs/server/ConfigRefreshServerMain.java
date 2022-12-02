f997e9a793a09d48c9a645c8447170ba3f98af0b
mdtamim <tamim.291@gmail.com>
2022-12-02 04:32:50 -0800

    initial commit

---
+++ src/main/java/com/tamim/crs/server/ConfigRefreshServerMain.java
@@ -0,0 +1,11 @@
+package com.tamim.crs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRefreshServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigRefreshServerMain.class, args);
+    }
+}
